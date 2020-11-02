40ceae7abbc46f47dc65bb37d673e5ccc37e8a23
Akash Jain <akashjain971@gmail.com>
2020-11-02 21:41:53 +0530

    feat: clone to a particular timestamp

--- src/commands/restore-backend.ts
+++ src/commands/restore-backend.ts
@@ -4,9 +4,15 @@ import {flags} from '@oclif/command'
 import {cli} from 'cli-ux'
 
 const RESTORE_MUTATION  = `
-mutation($uid: String!) {
-  restore(uid: $uid) {
-    response { code message restoreId }
+mutation($uid: String!, $backupFolder: String, $backupNum: Int) {
+  restore(uid: $uid, backupFolder: $backupFolder, backupNum: $backupNum) {
+    response {
+      code
+      message
+      restoreId
+    }, errors {
+      message
+    }
   }
 }`
 
@@ -14,13 +20,15 @@ export default class RestoreBackend extends BaseCommand {
     static description = 'Restore into a backend by source backend ID'
 
     static examples = [
-      '$ slash-graphql restore-backend -e https://clone.cloud.dgraph.io/graphql -t <apiToken> --source <source backend id or url>',
+      '$ slash-graphql restore-backend -e https://clone.cloud.dgraph.io/graphql -t <apiToken> --source <source backend id or url> [-f <backup folder> -n <backup number>]',
     ]
 
     static flags = {
       ...BaseCommand.commonFlags,
       ...BaseCommand.endpointFlags,
       source: flags.string({char: 's', description: 'Source backend ID or url to get the data to be restored', required: true}),
+      backupFolder: flags.string({char: 'f', description: 'Backup folder retrieved from list-backups. Defaults to ""(latest).', required: false, default: ''}),
+      backupNum: flags.integer({char: 'n', description: 'Backup number retrieved from list-backups. Defaults to 0(latest).', required: false, default: 0}),
       confirm: flags.boolean({char: 'y', description: 'Skip Confirmation', default: false}),
     }
 
@@ -44,12 +52,21 @@ export default class RestoreBackend extends BaseCommand {
         return
       }
 
-      const {errors, data} = await backend.slashAdminQuery<{restore: {response: {code: string; message: string; restoreId: number}}}>(RESTORE_MUTATION, {
+      const {errors, data} = await backend.slashAdminQuery<{restore: {response: {code: string; message: string; restoreId: number}; errors: [{ message: string }]}}>(RESTORE_MUTATION, {
         uid: sourceID,
+        backupFolder: opts.flags.backupFolder,
+        backupNum: opts.flags.backupNum,
       })
 
       if (errors) {
         for (const {message} of errors) {
+          this.error(message)
+        }
+        return
+      }
+
+      if (data.restore.errors) {
+        for (const {message} of data.restore.errors) {
           this.error(message)
         }
         return
@@ -60,7 +77,7 @@ export default class RestoreBackend extends BaseCommand {
       if (opts.flags.quiet) {
         this.log('Restore key: ' + restoreId)
       } else {
-        this.log('Restore in progress. Please run "slash-graphql restore-backend-status" to fetch the current status, using the following key ', restoreId)
+        this.log('Restore in progress. Please run "slash-graphql restore-backend-status" to fetch the current status, using the following key', restoreId)
       }
     }
 }
