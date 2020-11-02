03ab8271dc7825197fc38d1c67426a7ec5a55d6e
Akash Jain <akashjain971@gmail.com>
2020-11-02 21:41:53 +0530

    feat: clone to a particular timestamp

--- src/commands/list-backups.ts
+++ src/commands/list-backups.ts
@@ -5,32 +5,15 @@ const LIST_BACKUPS_QUERY = `
 query {
     listBackups {
         response {
-            path
             type
             backupNum
+            folder
+            timestamp
         }, errors {
             message
         }
     }
 }`
-
-const getBackupTimeStamp = (path: string) => {
-  // example: /0xb/2020-41/dgraph.20201013.081611.623/manifest.json
-  const timeregex = path.match(new RegExp('/dgraph.(.*)/manifest.json'))
-  if (!timeregex) return ''
-
-  // format: YYYYMMDD.HHMMSS.Milliseconds
-  // example: 20201013.100403.500
-  const timestr = timeregex[1]
-  const year = parseInt(timestr.substr(0, 4), 10)
-  const month = parseInt(timestr.substr(4, 2), 10) - 1 // month is 0 indexed
-  const date = parseInt(timestr.substr(6, 2), 10)
-  const hour = parseInt(timestr.substr(9, 2), 10)
-  const minute = parseInt(timestr.substr(11, 2), 10)
-  const second = parseInt(timestr.substr(12, 2), 10)
-
-  return new Date(Date.UTC(year, month, date, hour, minute, second)).toString()
-};
 
 export default class ListBackups extends BaseCommand {
   static description = 'List all backups of the current backend'
@@ -61,6 +44,6 @@ export default class ListBackups extends BaseCommand {
     const response: any = data.listBackups.response
     const backupsList = [].concat(...[].concat(...response).reverse())
 
-    cli.table(backupsList, {Id: {get: ({backupNum}) => backupNum}, Type: {get: ({type}) => type}, Timestamp: {get: ({path}) => getBackupTimeStamp(path)}}, {printLine: this.log, ...opts.flags})
+    cli.table(backupsList, {Timestamp: {get: ({timestamp}) => timestamp}, Type: {get: ({type}) => type}, BackupFolder: {get: ({folder}) => folder}, BackupNum: {get: ({backupNum}) => backupNum}}, {printLine: this.log, ...opts.flags})
   }
 }
