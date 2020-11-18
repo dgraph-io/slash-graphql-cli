904e9a173eddfe40bdd56b389560b23d824cbf48
Akash Jain <akashjain971@gmail.com>
2020-11-18 16:48:35 +0530

    seperate commands

---
+++ src/commands/lambda-logs.ts
@@ -0,0 +1,59 @@
+import {BaseCommand} from '../lib'
+import {getEnvironment} from '../lib/environments'
+import {flags} from '@oclif/command'
+
+const GET_LAMBDA_LOGS = `
+query GetLambdaLogs($input: LambdaLogsInput!) {
+  getLambdaLogs(input: $input)
+}
+`
+
+export default class LambdaLogs extends BaseCommand {
+  static description = 'Get the Lambda script associated with the backend.'
+
+  static examples = [
+    '$ slash-graphql lambda-logs -e https://frozen-mango.cloud.dgraph.io/graphql',
+    '$ slash-graphql lambda-logs -e 0x1234 -h 5',
+  ]
+
+  static flags = {
+    ...BaseCommand.commonFlags,
+    ...BaseCommand.endpointFlags,
+    hours: flags.integer({char: 'h', description: 'Show lambda logs for last given hours. Defaults to 1 hour.', required: false, default: 1}),
+  }
+
+  async run() {
+    const opts = this.parse(LambdaLogs)
+    const {apiServer, authFile} = getEnvironment(opts.flags.environment)
+    let endpoint = opts.flags.endpoint || ''
+
+    const token = await this.getAccessToken(apiServer, authFile)
+    if (!token) {
+      this.error('Please login with `slash-graphql` login')
+    }
+
+    if (!endpoint.match(/0x[0-9a-f]+/)) {
+      const backend = await this.findBackendByUrl(apiServer, token, endpoint)
+      endpoint = backend?.uid || ''
+    }
+
+    const start = new Date()
+    start.setHours(start.getHours() - opts.flags.hours)
+    const {errors, data} = await this.sendGraphQLRequest(apiServer, token, GET_LAMBDA_LOGS, {
+      input: {
+        deploymentID: endpoint,
+        start: start.toISOString(),
+      },
+    })
+
+    if (errors) {
+      for (const {message} of errors) {
+        this.error(message)
+      }
+      return
+    }
+
+    const logs = data.getLambdaLogs.join('\n')
+    this.log(logs || 'No logs')
+  }
+}
---
+++ src/commands/update-lambda.ts
@@ -0,0 +1,43 @@
+import {BaseCommand} from '../lib'
+import {getEnvironment} from '../lib/environments'
+import {flags} from '@oclif/command'
+import fs from 'fs'
+
+export default class LambdaLogs extends BaseCommand {
+    static description = 'Get the Lambda script associated with the backend.'
+
+    static examples = [
+      '$ slash-graphql update-lambda -e https://frozen-mango.cloud.dgraph.io/graphql -f <filepath>',
+      '$ slash-graphql update-lambda -e 0x1234 -f /home/user/Downloads/script.js',
+    ]
+
+    static flags = {
+      ...BaseCommand.commonFlags,
+      ...BaseCommand.endpointFlags,
+      file: flags.string({char: 'f', description: 'Lambda script file path.', required: true}),
+    }
+
+    async run() {
+      const opts = this.parse(LambdaLogs)
+      const {apiServer, authFile} = getEnvironment(opts.flags.environment)
+      let endpoint = opts.flags.endpoint || ''
+
+      const token = await this.getAccessToken(apiServer, authFile)
+      if (!token) {
+        this.error('Please login with `slash-graphql` login')
+      }
+
+      if (!endpoint.match(/0x[0-9a-f]+/)) {
+        const backend = await this.findBackendByUrl(apiServer, token, endpoint)
+        endpoint = backend?.uid || ''
+      }
+
+      const data = fs.readFileSync(opts.flags.file, 'utf8')
+      const lambdaScript = Buffer.from(data).toString('base64')
+      const {error, response} = await this.patchBackend(apiServer, token, endpoint, {lambdaScript})
+      if (error) {
+        this.error(error)
+      }
+      this.log(response.message)
+    }
+}
